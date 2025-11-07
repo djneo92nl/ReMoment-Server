4da5f436fec3110915faa82010ad44383405cd38
Remko Janse <remko@pionect.nl>
2025-11-07 13:05:16 +0100

    Initial Commit init

---
+++ app/Console/Commands/DeviceDiscovery.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeviceDiscovery extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:device-discovery';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
