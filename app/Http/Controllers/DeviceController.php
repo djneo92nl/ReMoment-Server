3f26760adbb8486dcf8d9b69640db9f4965f5b61
Remko Janse <remko@pionect.nl>
2025-12-25 15:22:55 +0100

    Show devices on web interface

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -12,7 +12,9 @@ class DeviceController extends Controller
      */
     public function index()
     {
-        //
+        return view('devices.index', [
+            'devices' => Device::paginate(10),
+        ]);
     }
 
     /**
---
+++ resources/views/components/device/list-view.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-white  dark:bg-gray-800 rounded-lg px-6 py-4 ring shadow-xl ring-gray-900/5">
+    <h3 class="text-gray-900 dark:text-white  text-base font-medium tracking-tight ">{{$device->device_name}}</h3>
+
+</div>
